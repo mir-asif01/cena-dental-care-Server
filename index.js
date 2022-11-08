978ca2d68f73dad7e6a9cc84e9c781a4cd7c4323
asif3062 <asifbsb2002@gmail.com>
2022-11-08 20:22:26 +0600

    services and single service url genarated

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json())
@@ -26,6 +27,12 @@ async function run() {
         const cursor = serviceCollection.find(query)
         const services = await cursor.toArray()
         res.send(services)
+    })
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: ObjectId(id) }
+        const service = await serviceCollection.findOne(query)
+        res.send(service)
     })
   } finally {
    
