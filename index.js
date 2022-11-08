fb09adb68c4b7d58d35e7ae4c7f00360fd3df0c1
asif3062 <asifbsb2002@gmail.com>
2022-11-08 20:12:07 +0600

    mongodb set up completed

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/',(req,res)=>{
     res.send('Cena dental care server running');
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mtnbd39.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    // const serviceCollection = client.db('geniusCarDB').collection('services')
+    const serviceCollection = client.db('cena_dental_care').collection('services')
+  try {
+    app.get('/services',async(req,res)=>{
+        const query = {}
+        const cursor = serviceCollection.find(query)
+        const services = await cursor.toArray()
+        res.send(services)
+    })
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port,()=>{
     console.log(`This is app running on port ${port}`)
