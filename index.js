6c968672b711ba7f3f8422df5f89e5011a6b1445
asif3062 <asifbsb2002@gmail.com>
2022-11-10 13:14:21 +0600

    my reviews url loaded

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server running');
 })
 
@@ -20,58 +20,68 @@ async function run() {
     // const serviceCollection = client.db('geniusCarDB').collection('services')
     const serviceCollection = client.db('cena_dental_care').collection('services')
     const reviewCollection = client.db('cena_dental_care').collection('reviews')
-  try {
+    try {
 
-    // homepage services
-    app.get('/home',async(req,res)=>{
-        const query = {}
-        const cursor = serviceCollection.find(query).limit(3)
-        const homepageServices = await cursor.toArray()
-        res.send(homepageServices)
-    })
+        // homepage services
+        app.get('/home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3)
+            const homepageServices = await cursor.toArray()
+            res.send(homepageServices)
+        })
 
-    //services
-    app.get('/services',async(req,res)=>{
-        const query = {}
-        const cursor = serviceCollection.find(query)
-        const services = await cursor.toArray()
-        res.send(services)
-    })
-    app.get('/services/:id', async (req, res) => {
-        const id = req.params.id
-        const query = { _id: ObjectId(id) }
-        const service = await serviceCollection.findOne(query)
-        res.send(service)
-    })
+        //services
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
 
-    // reviews
+        // reviews
 
-    app.post('/review', async(req,res)=>{
-        const review = req.body
-        const result = await reviewCollection.insertOne(review)
-        res.send(result)
-    })
+        app.post('/review', async (req, res) => {
+            const review = req.body
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
 
-    app.get('/reviews',async(req,res)=>{
-        let query = {}
-        if(req?.query.serviceId){
-            query = {
-                serviceId : req.query.serviceId
+        app.get('/reviews', async (req, res) => {
+            let query = {}
+            console.log(req?.query.email, req?.query.serviceId)
+            if (req?.query.serviceId) {
+                query = {
+                    serviceId: req.query.serviceId
+                }
+                const cursor = reviewCollection.find(query).sort({ ReviewAddedOn: -1 })
+                const reviews = await cursor.toArray()
+                res.send(reviews)
             }
-        }
-        const cursor = reviewCollection.find(query).sort({ReviewAddedOn : -1})
-        const reviews = await cursor.toArray()
-        res.send(reviews)
-    })
-  } finally {
-   
-  }
+            if (req?.query.email) {
+                query = {
+                    userEmail: req.query.email
+                }
+                const cursor = reviewCollection.find(query)
+                const reviews = await cursor.toArray()
+                res.send(reviews)
+            }
+
+        })
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`This is app running on port ${port}`)
 })
